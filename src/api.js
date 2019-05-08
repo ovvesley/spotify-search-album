0b3d4c6a115b2a3fc582a6b25c3df3854b865f69
wesley101201 <wsf.ley@gmail.com>
2019-05-08 18:44:24 -0300

    UPDATE API

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const KEY = 'BQB8C_ddRE3bOYZK4lW0tcmVHjUy6Dcjb3M5eJ5hjHt_w_N0SgipsavKcBCq5FzUsMKfN6H'
+const KEY = 'BQBBeLJlnfO2PoOtFcbKatkSsD_CfOwVi8Gnsr25-DYvH3bt8Hd2pUtunnvviAENH9UPHHwfrqgdmOBu16hBV4F4Mk168wEjcAjTtG8X1KUob0YOUHgcR_OHZfmlBGGELa3aQQ5US1qN7IhqQRaCCzOIF2zUYgU5NxxlvvxHwtWFkCYVNyEkNsPNR1BxcO_2VTRFNnTlWgGxOhIHDS7OGQRpjcRy5abLtDh8MHoQip9YRW4_9CApWj37eFWn-iZemDxUjBGuPCiCeWrpPhaflA'
 let search = (query) => {
     const api = axios.create({
         headers: {
